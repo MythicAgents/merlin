0e908ab57ddcc9f369000e67b1aeb073a2bdab30
Ne0nd0g <Russel.VanTuyl@gmail.com>
2021-08-09 17:36:24 -0400

    Garble support and v1.1.0 commands

---
+++ Payload_Type/merlin/mythic/agent_functions/ps.py
@@ -0,0 +1,50 @@
+
+from merlin import MerlinJob
+from mythic_payloadtype_container.MythicCommandBase import *
+from mythic_payloadtype_container.MythicRPC import *
+import json
+
+# Set to enable debug output to Mythic
+debug = False
+
+
+class PSArguments(TaskArguments):
+    def __init__(self, command_line):
+        super().__init__(command_line)
+        self.args = {}
+
+    async def parse_arguments(self):
+        pass
+
+
+class PSCommand(CommandBase):
+    cmd = "ps"
+    needs_admin = False
+    help_cmd = "ps"
+    description = "Enumerates a list of running processes (WINDOWS ONLY)."
+    version = 1
+    author = "@Ne0nd0g"
+    argument_class = PSArguments
+    attackmapping = []
+    attributes = CommandAttributes(
+        spawn_and_injectable=False,
+        supported_os=[SupportedOS.Windows]
+    )
+
+    async def create_tasking(self, task: MythicTask) -> MythicTask:
+        task.display_params = f'{self.cmd}'
+
+        command = {
+            "command": self.cmd,
+        }
+
+        task.args.add_arg("type", MerlinJob.MODULE, ParameterType.Number)
+        task.args.add_arg("payload", json.dumps(command), ParameterType.String)
+
+        if debug:
+            await MythicRPC().execute("create_output", task_id=task.id, output=f'[DEBUG]Returned task:\r\n{task}\r\n')
+
+        return task
+
+    async def process_response(self, response: AgentResponse):
+        pass
